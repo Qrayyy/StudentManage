6b183a07f181bc4ad9090733fe9835dca177f8cc
caicaizi-s <74341584+caicaizi-debug@users.noreply.github.com>
2021-06-28 15:50:14 +0800

    Update DbUtils.java

--- src/main/java/top_caicaizi/utils/DbUtils.java
+++ src/main/java/top_caicaizi/utils/DbUtils.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class DbUtils {
     private static String driver = "com.mysql.cj.jdbc.Driver";
-    private static String url = "jdbc:mysql://localhost:3306/keshe?characterEncoding=utf8";
-    private static String username = "root";
-    private static String password = "caicaizi";
+    private static String url = "jdbc:mysql://localhost:3306/****?characterEncoding=utf8";
+    private static String username = "****";
+    private static String password = "******";
     private static Connection con = null;
 
     public static Connection getConnection(){
